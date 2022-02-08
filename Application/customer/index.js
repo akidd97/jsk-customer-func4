f9461eaf1325f23152af5b1d80bd7bb944cb6c3f
Alan Kidd <akidd97@hotmail.com>
2022-02-09 08:50:41 +1000

    Update index.js

--- Application/customer/index.js
+++ Application/customer/index.js
@@ -3,7 +3,7 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
     if (name == "Azure") {
-    var responseMessage = '[{"CustomerID":123,"CustomerName":"Tailspin Toys (Roe Park, NY)","PhoneNumber":"(212) 555-0100","FaxNumber":"(212) 555-0101","WebsiteURL":"http://www.tailspintoys.com/RoePark","Delivery":{"AddressLine1":"Shop 219","AddressLine2":"528 Persson Road","PostalCode":"90775"}}]';
+    var responseMessage = '[{"CustomerID":123,"CustomerName":"Tailspin Toads (Roe Park, NY)","PhoneNumber":"(212) 555-0100","FaxNumber":"(212) 555-0101","WebsiteURL":"http://www.tailspintoys.com/RoePark","Delivery":{"AddressLine1":"Shop 219","AddressLine2":"528 Persson Road","PostalCode":"90775"}}]';
     } else {
     var responseMessage = '[{"CustomerID":124,"CustomerName":"Tailspin Toys (Roe Park, NY)","PhoneNumber":"(212) 555-0100","FaxNumber":"(212) 555-0101","WebsiteURL":"http://www.tailspintoys.com/RoePark","Delivery":{"AddressLine1":"Shop 219","AddressLine2":"528 Persson Road","PostalCode":"90775"}}]';
     }
