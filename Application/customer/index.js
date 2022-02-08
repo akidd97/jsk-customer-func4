ff41fe2d8f48a6b0bcc5d3f23584c891025bd6c5
Alan Kidd <akidd97@hotmail.com>
2022-02-08 19:47:15 +1000

    Change customer code

--- Application/customer/index.js
+++ Application/customer/index.js
@@ -2,9 +2,11 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    if (name == "Azure") {
+    var responseMessage = '[{"CustomerID":123,"CustomerName":"Tailspin Toys (Roe Park, NY)","PhoneNumber":"(212) 555-0100","FaxNumber":"(212) 555-0101","WebsiteURL":"http://www.tailspintoys.com/RoePark","Delivery":{"AddressLine1":"Shop 219","AddressLine2":"528 Persson Road","PostalCode":"90775"}}]';
+    } else {
+    var responseMessage = '[{"CustomerID":124,"CustomerName":"Tailspin Toys (Roe Park, NY)","PhoneNumber":"(212) 555-0100","FaxNumber":"(212) 555-0101","WebsiteURL":"http://www.tailspintoys.com/RoePark","Delivery":{"AddressLine1":"Shop 219","AddressLine2":"528 Persson Road","PostalCode":"90775"}}]';
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
