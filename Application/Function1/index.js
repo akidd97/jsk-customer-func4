2ce4cbd0baa3c809f63dd163a1b53401aa904470
Alan Kidd <akidd97@hotmail.com>
2022-02-08 16:11:15 +1000

    Revert "Added customer responses"
    This reverts commit 3b748c03492a1c326035ed76b8779ac4738bacd8.

--- Application/Function1/index.js
+++ Application/Function1/index.js
@@ -1,15 +1,16 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    if (name == "Azure") {
-    var responseMessage = '[{"CustomerID":123,"CustomerName":"Tailspin Toys (Roe Park, NY)","PhoneNumber":"(212) 555-0100","FaxNumber":"(212) 555-0101","WebsiteURL":"http://www.tailspintoys.com/RoePark","Delivery":{"AddressLine1":"Shop 219","AddressLine2":"528 Persson Road","PostalCode":"90775"}}]';
-    } else {
-    var responseMessage = '[{"CustomerID":124,"CustomerName":"Tailspin Toys (Roe Park, NY)","PhoneNumber":"(212) 555-0100","FaxNumber":"(212) 555-0101","WebsiteURL":"http://www.tailspintoys.com/RoePark","Delivery":{"AddressLine1":"Shop 219","AddressLine2":"528 Persson Road","PostalCode":"90775"}}]';
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.name || req.body.name) + "! Welcome to Azure Functions!"
+        };
     }
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+    else {
+        context.res = {
+            status: 200,
+            body: "Hello there! Welcome to Azure Functions!"
+        };
+    }
+};
